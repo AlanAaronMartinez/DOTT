e555d8ff537d483f482a0fb331cd2912163906ff
Alan Aarón Martínez Castillo <95430051+AlanAaronMartinez@users.noreply.github.com>
2021-12-22 14:29:39 -0600

    cidr_to_mask Fixed

--- cidr_convert_api/python/convert.py
+++ cidr_convert_api/python/convert.py
@@ -1,8 +1,15 @@
 # These functions need to be implemented
+import socket
+import struct
+
 class CidrMaskConvert:
 
     def cidr_to_mask(self, val):
+        network, net_bits = self.split('/')
+        host_bits = 32 - int(net_bits)
+        val = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
         return val
+        
 
     def mask_to_cidr(self, val):
         return val
