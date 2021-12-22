5abe83a50d4849b2bae87baa8ed0f806ea69013f
Alan Aarón Martínez Castillo <95430051+AlanAaronMartinez@users.noreply.github.com>
2021-12-22 17:46:48 -0600

    as

--- cidr_convert_api/python/convert.py
+++ cidr_convert_api/python/convert.py
@@ -8,10 +8,11 @@ import ipaddress
 class CidrMaskConvert:
 
     def cidr_to_mask(self, val):
+            print('LINEA:',  val)
+            print('LINEA:',  int(val))
         if int(val) >= 1 and int(val) <= 32:
             host_bits = 32 - int(val)
             val = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
-            print('LINEA: ',self, val)
             return str(val)
         else:
             return 'Invalid'
